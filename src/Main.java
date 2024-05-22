4b8ffbba73b54e89f25ddda27c2593a8f665a67a
Mihay Victor <m.victorcezario@gmail.com>
2024-05-22 01:21:39 -0300

    Files

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double celsius, fahrenheit, kelvin;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("*** Seja bem-vindo ao Temperature Converter ***");
+        System.out.print("Digite uma temperatura em graus Celsius: ");
+        celsius = input.nextDouble();
+        fahrenheit = celsius * 1.8 + 32;
+        kelvin = celsius + 273.15;
+        System.out.printf("%.1f °C (Celsius) | %.1f °F (fahrenheit) | %.1f K (kelvin)\n", celsius, fahrenheit, kelvin);
+    }
+}
